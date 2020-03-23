e94c3ee203f3ef3f384855b1abbf39301cc2bba0
Tales Pereira <talesdsp@gmail.com>
2020-03-23 13:23:11 -0300

    finalize welcome screns

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,27 +1,81 @@
 import React from "react";
 import { Image } from "react-native";
-import { background } from "../constants/Images";
+import { ScrollView } from "react-native-gesture-handler";
+import Block from "../components/Block";
+import Text from "../components/Text";
+import { images, theme } from "../constants/";
 
-export default function Welcome() {
+const { background } = images;
+const { COLORS, SIZES } = theme;
+
+const backgrounds = [
+  {
+    title: "Secured, forever.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius omnis molestiae sed fuga est commodi voluptatibus perferendis debitis nemo labore.",
+    img: background.image,
+  },
+  {
+    title: "Secured, forever.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius omnis molestiae sed fuga est commodi voluptatibus perferendis debitis nemo labore.",
+    img: background.image,
+  },
+  {
+    title: "Secured, forever.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius omnis molestiae sed fuga est commodi voluptatibus perferendis debitis nemo labore.",
+    img: background.image,
+  },
+];
+
+export default function Welcome({ navigation }) {
+  const renderImages = () => (
+    <ScrollView
+      horizontal
+      pagingEnabled
+      scrollEnabled
+      decelerationRate={0}
+      scrollEventThrottle={16}
+      snapToAlignment="center"
+      showsHorizontalScrollIndicator={false}
+    >
+      {backgrounds.map((item, index) => (
+        <Block key={`img-${index}`} center bottom style={{ width: SIZES.width }}>
+          <Image source={item.img} resizeMode="center" style={{ width: "75%", height: "100%" }} />
+        </Block>
+      ))}
+    </ScrollView>
+  );
+
+  const renderDots = () => (
+    <Block flex={false} row center middle margin={[20, 0, 40, 0]}>
+      {[...Array(3)].map((_, index) => (
+        <Block
+          key={`dot-${index}`}
+          flex={false}
+          gray
+          margin={[0, 5]}
+          radius={3}
+          style={{ width: 8, height: 8 }}
+          color={rgba(COLORS.gray, 0.5)}
+        />
+      ))}
+    </Block>
+  );
+
   return (
     <Block safe>
       <Block center middle>
-        <Image
-          source={background.welcome}
-          resizeMode="center"
-          style={{ width: "75%", height: "100%" }}
-        />
+        {renderImages()}
       </Block>
       <Block flex={0} center bottom margin={60}>
-        <Text h3 semibold>
-          Secured, Forever.
-        </Text>
-        <Text h3 semibold center caption gray margin={[10, 0]}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta in eos, doloremque eligendi
-          et rerum beatae facere ut assumenda at, autem quaerat soluta consectetur ipsum?
-          Accusantium recusandae quas rem possimus.
-        </Text>
-        <Button primary>
+        <Text h3 semibold></Text>
+        <Text h3 semibold center caption gray margin={[10, 0]}></Text>
+
+        {renderDots()}
+
+        <Button primary onPress={() => navigation.navigate("VPN")}>
           <Text center white caption bold margin={[5, 21]}>
             GET STARTED
           </Text>
