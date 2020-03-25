27ca148f3116b03b4c321ef203d82bb377092710
Tales Pereira <talesdsp@gmail.com>
2020-03-24 22:48:33 -0300

    setup servers

---
+++ constants/Servers.js
@@ -0,0 +1,36 @@
+import { icons } from "./Images";
+
+export const servers = [
+  {
+    name: "Automatic",
+    icon: (props) => icons.automatic(props),
+  },
+  {
+    name: "New York",
+    icon: (props) => icons.us(props),
+  },
+  {
+    name: "London",
+    icon: (props) => icons.uk(props),
+  },
+  {
+    name: "Moscow",
+    icon: (props) => icons.russia(props),
+  },
+  {
+    name: "Malmö",
+    icon: (props) => icons.sweden(props),
+  },
+  {
+    name: "Melbourne",
+    icon: (props) => icons.australia(props),
+  },
+  {
+    name: "New Delhi",
+    icon: (props) => icons.india(props),
+  },
+  {
+    name: "Singapore",
+    icon: (props) => icons.singapore(props),
+  },
+];
