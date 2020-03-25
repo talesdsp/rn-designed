8c476cf46b6e0eac39825e235c984701805a3984
Tales Pereira <talesdsp@gmail.com>
2020-03-25 00:03:58 -0300

    cleaning app.js

--- App.js
+++ App.js
@@ -1,68 +1,13 @@
-import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { SplashScreen } from "expo";
-import * as Font from "expo-font";
-import * as React from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import useLinking from "./navigation/useLinking";
-import VPN from "./screens/Vpn";
-import Welcome from "./screens/Welcome";
+import React from "react";
+import { Platform, StatusBar } from "react-native";
+import "react-native-gesture-handler";
+import Navigation from "./navigation/";
 
-const Stack = createStackNavigator();
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={Welcome} />
-            <Stack.Screen name="Root" component={VPN} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+      <Navigation />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
