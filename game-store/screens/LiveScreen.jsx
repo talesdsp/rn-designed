1dc9692300476a55e46ecc039e8cfea5cd0a957f
tales <talesdsp@gmail.com>
2020-07-05 12:00:27 -0300

    add LiveScreen

---
+++ game-store/screens/LiveScreen.jsx
@@ -0,0 +1,156 @@
+import React from "react"
+import { StatusBar } from "react-native"
+import styled from "styled-components/native"
+import Text from "../components/Text"
+import games from "../game-data"
+
+const LiveScreen = () => {
+  return (
+    <Container>
+      <StatusBar barStyle="light-content" />
+
+      <Header>
+        <Text large bold>
+          Streaming
+        </Text>
+
+        <SearchContainer>
+          <Search placeholder="Search live streams or games..." placeholderTextColor="#838383" />
+          <SearchIcon>
+            <Entypo name="magnifying-glass" size={24} color="#838383" />
+          </SearchIcon>
+        </SearchContainer>
+      </Header>
+
+      <SectionContainer>
+        <Text medium bold>
+          Popular Games
+        </Text>
+
+        <PopularGames horizontal showsHorizontalScrollIndicator={false}>
+          {games.map((game, index) => (
+            <PopularGameContainer key={index}>
+              <PopularGame source={game.cover} />
+            </PopularGameContainer>
+          ))}
+        </PopularGames>
+      </SectionContainer>
+
+      <SectionContainer>
+        <LiveGamesTitle>
+          <Text medium bold>
+            Live now
+          </Text>
+          <Text bold color="#819ee5">
+            View all
+          </Text>
+        </LiveGamesTitle>
+      </SectionContainer>
+
+      <LiveGames
+        data={games}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={(item) => (
+          <LiveGameContainer>
+            <LiveGame source={item.cover} />
+            <LiveGameOver>
+              <LiveGameTitle>
+                <Text>{item.title}</Text>
+              </LiveGameTitle>
+
+              <LiveGameBadge>
+                <Text small bold>
+                  Live
+                </Text>
+              </LiveGameBadge>
+            </LiveGameOver>
+          </LiveGameContainer>
+        )}
+      />
+    </Container>
+  )
+}
+
+export default LiveScreen
+
+const Container = styled.SafeAreaView`
+  background-color: #343434;
+  flex: 1;
+`
+
+const Header = styled.View`
+  margin: 32px 16px 0 16px;
+`
+
+const SearchContainer = styled.View`
+  position: relative;
+  margin: 32px 8px;
+  background-color: #404040;
+  border-radius: 100px;
+  justify-content: center;
+`
+
+const Search = styled.TextInput`
+  padding: 16px 64px 16 32px;
+  color: #c6c6c6;
+`
+
+const SearchIcon = styled.TouchableOpacity`
+  position: absolute;
+  right: 16px;
+`
+const SectionContainer = styled.View``
+
+const PopularGames = styled.ScrollView`
+  margin: 16px 0 16px -16px;
+`
+
+const PopularGameContainer = styled.View`
+  width: 80px;
+  margin: 0 16px;
+`
+
+const PopularGame = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 12px;
+`
+const LiveGamesTitle = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const LiveGames = styled.Flatlist`
+  flex: 1;
+  margin: 16px 16px 0 16px;
+`
+const LiveGameContainer = styled.View``
+
+const LiveGame = styled.Image`
+  width: 100%;
+  height: 275px;
+  margin-bottom: 16px;
+  border-radius: 12px;
+`
+
+const LiveGameOver = styled.View`
+  position: absolute;
+  flex-direction: row;
+  align-items: center;
+  top: 8px;
+  right: 16px;
+`
+
+const LiveGameTitle = styled.View`
+  background-color: #819ee5;
+  border-radius: 4px;
+  padding: 2px 8px;
+  margin-right: 4px;
+`
+
+const LiveGameBadge = styled.View`
+  background-color: #e54949;
+  border-radius: 4px;
+  padding: 2px 8px;
+`
