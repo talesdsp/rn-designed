370125c5214f176dad30f1851473e9abd0332b25
tales <talesdsp@gmail.com>
2020-07-04 23:51:04 -0300

    game-data

---
+++ game-store/game-data.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    id: 1,
+    title: "Unforeseen Incidents",
+    teaser: "A classical style interactive mistery set in a beautiful hand-painted world",
+    description:
+      "Unforeseen Incidents is a classical style interactive mistery set in a beautiful hand-painted world",
+    rating: 4.8,
+    category: ["Action", "Indie"],
+    age: "12+",
+    cover: require("./assets/images/unforeseen-1.jpg"),
+    screenshots: [
+      require("./assets/images/unforeseen-2.jpg"),
+      require("./assets/images/unforeseen-3.jpg"),
+      require("./assets/images/unforeseen-4.jpg"),
+    ],
+    backgroundColor: "#824571",
+  },
+  {
+    id: 2,
+    title: "Ori and the Will of the Whisps",
+    teaser:
+      "Embark on a new journey in a vast, exotic world where you'll encounter towering enemies.",
+    description:
+      "Play the critically acclaimed masterpiece. Embark on a new journey in a vast, exotic world where you'll encounter towering enemies",
+    rating: 3.8,
+    category: ["Strategy", "Indie", "Action"],
+    age: "11+",
+    cover: require("./assets/images/ori-1.jpg"),
+    screenshots: [
+      require("./assets/images/ori-2.jpg"),
+      require("./assets/images/ori-3.jpg"),
+      require("./assets/images/ori-4.jpg"),
+    ],
+    backgroundColor: "#456382",
+  },
+  {
+    id: 3,
+    title: "Unravel",
+    teaser: "Yarny, a character made from a single thead of yarn.",
+    description:
+      "Unravel is a physics-based puzzle platformer. Yarny, a character made from a single thread of yarn.",
+    rating: 2.4,
+    category: ["Casual", "Indie", "Adventure"],
+    age: "13+",
+    cover: require("./assets/images/unravel-1.jpg"),
+    screenshots: [
+      require("./assets/images/unravel-2.jpg"),
+      require("./assets/images/unravel-3.jpg"),
+      require("./assets/images/unravel-4.jpg"),
+    ],
+    backgroundColor: "#825d45",
+  },
+]
